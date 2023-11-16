60a46487e5d32966f238e5f49b65cfe453c674df
Fernando Vargas <fvargas320@gmail.com>
2023-11-15 23:51:19 -0800

    Fixed errors

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { ThemeProvide, AmplifyProvider } from "@aws-amplify/ui-react";
+import { AmplifyProvider } from "@aws-amplify/ui-react";
 
 import { Amplify } from 'aws-amplify';
 
 import awsconfig from './aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
-import { studioTheme } from "./components/ui-components";
 import theme from "./theme";
 import {ThemeProvider} from "@mui/material";
 
