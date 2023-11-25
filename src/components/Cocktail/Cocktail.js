5f329f16662f577bdc91b3573a4d148875666f97
Brian Bales <48467575+brianbales99@users.noreply.github.com>
2023-11-25 15:27:46 -0800

    Added a list view for cocktails
    list view is on the home page

--- src/components/Cocktail/Cocktail.js
+++ src/components/Cocktail/Cocktail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Cocktail(props) {
     let navigate = useNavigate();
@@ -9,16 +9,17 @@ function Cocktail(props) {
 
     return (
         <div onClick={navigateToCocktailCard}>
-            <div className="bg-white rounded-xl shadow-lg p-4 m-2">
-                <img className="block mx-auto h-52 rounded sm:mx-0 sm:shrink-0" src={props.image} alt={props.name} />
-                <p style={{ fontFamily: 'SFProRegular', color: "#000000", fontSize: "20px"}}>{props.name}</p>
-                <p style={{ fontFamily: 'SFProRegular', color: "#8A8A8D", fontSize: "16px"}}>{props.flavor} Flavor</p>
+            <div className="bg-white rounded-lg shadow-md p-2 m-1">
+                <img className="block mx-auto h-40 w-40 rounded-sm sm:mx-0 sm:shrink-0" src={props.image} alt={props.name} />
+                <p style={{ fontFamily: 'SFProRegular', color: "#000000", fontSize: "16px"}}>{props.name}</p>
+                <p style={{ fontFamily: 'SFProRegular', color: "#8A8A8D", fontSize: "12px"}}>{props.flavor} Flavor</p>
             </div>
         </div>
     );
 }
 
 export default Cocktail;
+
 
 
 
--- src/pages/MainPages/HomePage.js
+++ src/pages/MainPages/HomePage.js
@@ -1,27 +1,92 @@
 import React from 'react';
-import Cocktail from "../../components/Cocktail/Cocktail";
-import cocktailData from "../../data/cocktail_list.json"; // Adjust the path if necessary
+import { FixedSizeList } from 'react-window';
 
-const HomePage = (props) => {
+import Cocktail from "../../components/Cocktail/Cocktail";
+import cocktailData from "../../data/cocktail_list.json";
+
+const Row = ({ index, style }) => {
+    const cocktail = cocktailData[index];
+
+    return (
+        <div style={style}>
+            <Cocktail
+                id={cocktail.Cocktail_ID}
+                image={cocktail.image_url}
+                name={cocktail.name}
+                strength={cocktail.strength}
+                level={cocktail.difficulty_level}
+                flavor={cocktail.main_flavor}
+            />
+        </div>
+    );
+};
+
+const ClickableTitle = ({ onClick, children }) => (
+    <h2
+        style={{
+            fontFamily: 'SFProRegular',
+            color: "#000000",
+            fontSize: "32px",
+            cursor: 'pointer', // Add cursor pointer to indicate it's clickable
+            display: 'flex',
+            alignItems: 'center',
+            marginRight: '16px', // Add margin for spacing
+            transition: 'color 0.3s', // Add transition for smooth color change
+        }}
+        onClick={onClick}
+        onMouseEnter={(e) => e.target.style.color = '#007BFF'} // Change color on hover
+        onMouseLeave={(e) => e.target.style.color = '#000000'} // Revert color on mouse leave
+    >
+        {children}
+    </h2>
+);
+
+const HomePage = () => {
     return (
         <div>
-            <h2 style={{ fontFamily: 'SFProRegular', color: "#000000", fontSize: "32px"}}>Popular Cocktails</h2>
-            <div className="flex flex-wrap">
-                {cocktailData.map((cocktail) => {
-                    return (
-                        <Cocktail
-                            id = {cocktail.Cocktail_ID}
-                            image={cocktail.image_url}
-                            key={cocktail.key}
-                            name={cocktail.name}
-                            strength={cocktail.strength}
-                            level={cocktail.difficulty_level}
-                            flavor = {cocktail.main_flavor}
-                        />
-                    );
-                })}
+            {/* Popular Cocktails Section */}
+            <ClickableTitle
+                onClick={() => {
+                    // Handle the click event, e.g., navigate to the full list
+                    console.log('Clicked Popular Cocktails');
+                }}
+            >
+                Popular Cocktails
+            </ClickableTitle>
 
-            </div>
+            {/* Popular Cocktails List */}
+            <FixedSizeList
+                height={225}
+                width={1500} // Adjust the width to your desired size
+                itemSize={250}
+                itemCount={cocktailData.length}
+                itemData={cocktailData}
+                layout="horizontal"
+            >
+                {Row}
+            </FixedSizeList>
+
+            {/* Fruity Cocktails Section */}
+            <ClickableTitle
+                onClick={() => {
+                    // Handle the click event, e.g., navigate to the full list
+                    console.log('Clicked Fruity Cocktails');
+                }}
+            >
+                Fruity Cocktails
+            </ClickableTitle>
+
+            {/* Fruity Cocktails List */}
+            <FixedSizeList
+                height={225}
+                width={1500} // Adjust the width to your desired size
+                itemSize={250}
+                itemCount={cocktailData.length}
+                itemData={cocktailData}
+                layout="horizontal"
+            >
+                {Row}
+            </FixedSizeList>
         </div>
     );
 };
