cb9180e4f54835f317cffddd9a44421fca03a8b6
Fernando Vargas <fvargas320@gmail.com>
2023-12-10 23:47:17 -0800

    Added algolia API

--- src/pages/NavigationPages/Discover.js
+++ src/pages/NavigationPages/Discover.js
@@ -1,15 +1,59 @@
 import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import { Hits, InstantSearch, SearchBox, Pagination } from 'react-instantsearch';
+import CocktailCard from '../../components/Cocktail/CocktailCard';
+import Box from '@mui/material/Box';  // Import Box from Material-UI
+import 'instantsearch.css/themes/satellite.css';
+import {Bolt} from "@mui/icons-material";
+
+const searchClient = algoliasearch(
+    'JU5G9QN5IC',
+    '5312903f128979f46c5bb4948bbda094'
+);
 
 const Discover = () => {
+    // Inline CSS for the cocktail list
+    const cocktailListStyle = {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        gap: '20px',
+        marginTop: '20px'  // Added some margin to the top for spacing
+    };
+
     return (
-        <>
-            <h2>
-                DISCOVER
-            </h2>
+        <InstantSearch searchClient={searchClient} indexName="newCocktails">
+            <h1>Discover Cocktails</h1>
 
-        </>
+            <SearchBox placeholder="Search for cocktails" />
 
+            {/* Apply the cocktailListStyle to this div */}
+            <div style={cocktailListStyle}>
+                <Hits hitComponent={HitItem} />
+            </div>
+
+            <Box sx={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+                <Pagination />
+            </Box>
+        </InstantSearch>
     );
+};
+
+
+const HitItem = ({ hit }) => {
+    // Extract relevant data from the hit and pass it to CocktailCard
+    const cocktailData = {
+        id: hit.Cocktail_ID,
+        image: hit.Image_url,
+        name: hit.Cocktail_Name,
+        strength: hit.Strength,
+        level: hit.Difficulty_Level,
+        flavor: hit.Main_Flavor,
+    };
+
+    return (
+            <CocktailCard {...cocktailData} />
+   );
 };
 
 export default Discover;
