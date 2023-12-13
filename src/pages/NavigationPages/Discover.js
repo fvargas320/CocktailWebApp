f563541cb103feba64e862786dcdcdd98a805b86
Fernando Vargas <fvargas320@gmail.com>
2023-12-12 20:43:14 -0800

    still broken

--- src/pages/NavigationPages/Discover.js
+++ src/pages/NavigationPages/Discover.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import { Hits, InstantSearch, SearchBox, Pagination } from 'react-instantsearch';
 import CocktailCard from '../../components/Cocktail/CocktailCard';
 import Box from '@mui/material/Box';  // Import Box from Material-UI
 import 'instantsearch.css/themes/satellite.css';
-import {Bolt} from "@mui/icons-material";
 
 const searchClient = algoliasearch(
     'JU5G9QN5IC',
     '5312903f128979f46c5bb4948bbda094'
 );
 
+let cocktailArray = []
 const Discover = () => {
     // Inline CSS for the cocktail list
     const cocktailListStyle = {
@@ -20,6 +20,9 @@ const Discover = () => {
         gap: '20px',
         marginTop: '20px'  // Added some margin to the top for spacing
     };
+
+
+
 
     return (
         <InstantSearch searchClient={searchClient} indexName="newCocktails">
@@ -50,6 +53,12 @@ const HitItem = ({ hit }) => {
         level: hit.Difficulty_Level,
         flavor: hit.Main_Flavor,
     };
+
+    console.log("HIT")
+    console.log(hit )
+    console.log("cocktail Data")
+
+    console.log(cocktailData)
 
     return (
             <CocktailCard {...cocktailData} />
